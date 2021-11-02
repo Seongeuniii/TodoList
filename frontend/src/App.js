ca2c74e362de7a56d6de4890efc25fa09a79b204
Seongeuniii <kse8538@naver.com>
2021-11-03 01:51:57 +0900

    Add TodoInsert.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 // import Axios from 'axios';
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
@@ -45,6 +45,37 @@ function App() {
     }
   ])
 
+  // 값을 변경해도 컴포넌트가 재렌더링되지 않는다.
+  const nextId = useRef(8);
+
+  const onInsert = useCallback( text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false
+    };
+    setTodos(todos.concat(todo));
+    nextId.current += 1
+  }, [todos])
+
+  return (
+    <>
+      <Calender/> 
+      <TodoTemplate >
+        <TodoInsert onInsert={onInsert}/>
+        <TodoList todos={todos}/>
+      </TodoTemplate>
+    </>
+  );
+}
+
+export default App;
+
+// 새로운 객체를 만들 때마다 id 값에 1씩 더해 주어야 하는데 id 값은 useRef를 사용하여 관리 
+// 여기서 useState가 아닌 useRef를 사용하여 컴포넌트에서 사용할 변수를 만드는 이유는 id 값은 렌더링되는 정보가 아니기 때문
+// 예를 들어 이 값은 화면에 보이지도 않고, 이 값이 바뀐다고 해서 컴포넌트가 리렌더링될 필요도 없다. 단순히 새로운 항목을 만들 때 참조되는 값일 뿐이다.
+// onInsert 함수는 컴포넌트의 성능을 아낄 수 있도록 useCallback으로 감싸 준다. props로 전달해야 할 함수를 만들 때는 useCallback을 사용하여 함수를 감싸는 것을 습관화해야한다.
+
   // useEffect (() => {
   //   Axios.get('http://localhost:5000')
   //   .then ((res) => {
@@ -55,17 +86,3 @@ function App() {
   //     console.log(e);
   // })
   // },[]);
-
-  console.log(todos)
-  return (
-    <>
-      <Calender/> 
-      <TodoTemplate >
-        <TodoInsert/>
-        <TodoList todos={todos}/>
-      </TodoTemplate>
-    </>
-  );
-}
-
-export default App;
--- frontend/src/components/TodoInsert.js
+++ frontend/src/components/TodoInsert.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import plus from '../img/plusbutton.png'
-import Axios from 'axios';
-import { useState } from 'react';
+// import Axios from 'axios';
+import { useState, useCallback } from 'react';
 
 const AddButton = styled.img`
   width: 45px;
@@ -12,31 +12,34 @@ const AddButton = styled.img`
 `
 
 // 여기서 input 상태관리하고, app 컴포넌트에는 todos 배열에 새로운 객체를 추가하는 함수를 만들어야함
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
 
   const [value, setValue] = useState('');
 
-  // const addSchedule = () => {
-  //   const newSchedule = prompt('새로운 일정을 입력하세요')
-  //   if (newSchedule) {
-  //     Axios.post('http://localhost:5000',{ newSchedule })
-  //     .then((response) => {
-  //         console.log(response)  
-  //     }).catch((error) => {
-  //         console.log(error)
-  //     })
-  //   }
-  // }
+  const onSubmit = useCallback(e => {
+    onInsert(value);
+    setValue('');
+    e.preventDefault();
+  }, [onInsert, value]);
 
-  const addSchedule = () => {
-    setValue(prompt('새로운 일정을 입력하세요'));
-  }
-
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
 
   return(
-    <AddButton onClick={addSchedule} src={plus} alt="일정추가"></AddButton>
-  )
+    // <form onSubmit={onSubmit}>
+    //   <AddButton onClick={addSchedule} src={plus} alt="일정추가"></AddButton>
+    // </form>
+    <form onSubmit={onSubmit}>
+      <input
+        placeholder='할 일을 입력하세요'
+        value={value}
+        onChange={onChange}
+      />
+      <button type='submit'>등록</button>
+    </form>
 
+  )
 }
 
 export default TodoInsert;
